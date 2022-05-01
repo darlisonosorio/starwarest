e15b82cd1070653073b702d63ddba9f5937b84c0
CS0179\darlison.osorio_cont <darlison.osorio@contasimples.com>
2022-05-01 14:23:09 -0400

    added log.

--- Providers/src/main/java/com/darlison/starwarest/source/SWApiSource.java
+++ Providers/src/main/java/com/darlison/starwarest/source/SWApiSource.java
@@ -1,11 +1,16 @@
 package com.darlison.starwarest.source;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-
+@Component
 public class SWApiSource {
 
     public static final String API_SOURCE = "https://swapi.dev/api";
+
+    private static final Logger logger = LoggerFactory.getLogger(SWApiSource.class);
 
     public static final RestTemplate source = new RestTemplate();
 
@@ -14,7 +19,13 @@ public class SWApiSource {
     }
 
     public static <T> T makeRequest(String uri, Class<T> type, Object... params) {
-        return source.getForObject(uri, type, params);
+        logger.info("EXTERNAL URL: " + uri);
+
+        T result = source.getForObject(uri, type, params);
+
+        logger.info("RESULT: " + result);
+
+        return result;
     }
 
 }
---
+++ Providers/src/main/java/com/darlison/starwarest/util/LogUtil.java
@@ -0,0 +1,10 @@
+package com.darlison.starwarest.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LogUtil {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LogUtil.class);
+
+}
