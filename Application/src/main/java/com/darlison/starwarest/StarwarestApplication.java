9033bf922912908fff939f23aa35851effb6a4fd
CS0179\darlison.osorio_cont <darlison.osorio@contasimples.com>
2022-04-30 20:15:21 -0400

    Application configuration added.

---
+++ Application/src/main/java/com/darlison/starwarest/StarwarestApplication.java
@@ -0,0 +1,13 @@
+package com.darlison.starwarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarwarestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarwarestApplication.class, args);
+	}
+
+}
---
+++ Application/src/test/java/com/darlison/starwarest/StarwarestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darlison.starwarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarwarestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
